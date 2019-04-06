f191755c0275cdb7c74b5432532ff40fb314fd92
palwinder sandhu <palwindersandhu367@gmail.com>
2019-04-06 16:50:04 -0500

    Commit

--- Mvc2/Controllers/HomeController.cs
+++ Mvc2/Controllers/HomeController.cs
@@ -13,142 +13,130 @@ namespace Mvc2.Controllers
 {
     public class HomeController : Controller
     {
-        private ApplicationDbContext DbContext;
-        private ProjectHelper ProjectHelper;
-        public HomeController()
-        {
-            DbContext = new ApplicationDbContext();
-            ProjectHelper = new ProjectHelper(DbContext);
-        }
+        //private ApplicationDbContext DbContext;
+        //private ProjectHelper ProjectHelper;
+        //public HomeController()
+        //{
+        //    DbContext = new ApplicationDbContext();
+        //    ProjectHelper = new ProjectHelper(DbContext);
+        //}
         public ActionResult Index()
         {
-            var userId = User.Identity.GetUserId();
-            var model = ProjectHelper.GetUsersProjects(userId)
-               .Select(p => new IndexProjectViewModel
-               {
-                   ProjectId = p.Projectid,
-                   ProjectName = p.ProjectName,
-                   DateCreated = p.DateCreated,
-                   DateUpdated = p.DateUpdated,
-                   Users = p.Users ?? new List<ApplicationUser>(),
-               }).ToList();
+            //var userId = User.Identity.GetUserId();
+            //var model = ProjectHelper.GetUsersProjects(userId)
+            //   .Select(p => new IndexProjectViewModel
+            //   {
+            //       ProjectId = p.Projectid,
+            //       ProjectName = p.ProjectName,
+            //       DateCreated = p.DateCreated,
+            //       DateUpdated = p.DateUpdated,
+            //       Users = p.Users ?? new List<ApplicationUser>(),
+            //   }).ToList();
 
-            return View(model);
+            return View();
         }
         
-        [Authorize(Roles="Admin,ProjectManager")]
-        public ActionResult AllProjects()
-        {
-            var model = ProjectHelper.GetAllProjects()
-               .Select(p => new IndexProjectViewModel
-               {
-                   ProjectId = p.Projectid,
-                   ProjectName = p.ProjectName,
-                   DateCreated = p.DateCreated,
-                   DateUpdated = p.DateUpdated,
-                   Users = p.Users ?? new List<ApplicationUser>(),
-               }).ToList();
+        //[Authorize(Roles="Admin,ProjectManager")]
+        //public ActionResult AllProjects()
+        //{
+        //    var model = ProjectHelper.GetAllProjects()
+        //       .Select(p => new IndexProjectViewModel
+        //       {
+        //           ProjectId = p.Projectid,
+        //           ProjectName = p.ProjectName,
+        //           DateCreated = p.DateCreated,
+        //           DateUpdated = p.DateUpdated,
+        //           Users = p.Users ?? new List<ApplicationUser>(),
+        //       }).ToList();
 
-            return View(model);
-        }
+        //    return View(model);
+        //}
 
-        [HttpGet]
-        [Authorize(Roles = "Admin,ProjectManager")]
-        public ActionResult CreateProject()
-        {
-            //PopulateViewBag();
-            return View();
-        }
+        //[HttpGet]
+        //[Authorize(Roles = "Admin,ProjectManager")]
+        //public ActionResult CreateProject()
+        //{
+        //    //PopulateViewBag();
+        //    return View();
+        //}
 
-        [HttpPost]
-        [Authorize(Roles = "Admin,ProjectManager")]
-        public ActionResult CreateProject(CreateProjectViewModel formData)
-        {
-            return SaveProject(null, formData);
-        }
+        //[HttpPost]
+        //[Authorize(Roles = "Admin,ProjectManager")]
+        //public ActionResult CreateProject(CreateProjectViewModel formData)
+        //{
+        //    return SaveProject(null, formData);
+        //}
 
-        private ActionResult SaveProject(int? id, CreateProjectViewModel formData)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
+        //private ActionResult SaveProject(int? id, CreateProjectViewModel formData)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View();
+        //    }
 
-            Project project;
-            var userId = User.Identity.GetUserId();
-            if (!id.HasValue)
-            {
-                project = new Project();
-                var applicationUser = DbContext.Users.FirstOrDefault(user => user.Id == userId);
-                if (applicationUser == null)
-                {
-                    return RedirectToAction(nameof(HomeController.Index));
-                }
-                project.Users.Add(applicationUser);
-                DbContext.ProjectDatabase.Add(project);
-            }
-            else
-            {
-                project = DbContext.ProjectDatabase.FirstOrDefault(p => p.Projectid == id);
+        //    Project project;
+        //    var userId = User.Identity.GetUserId();
+        //    if (!id.HasValue)
+        //    {
+        //        project = new Project();
+        //        var applicationUser = DbContext.Users.FirstOrDefault(user => user.Id == userId);
+        //        if (applicationUser == null)
+        //        {
+        //            return RedirectToAction(nameof(HomeController.Index));
+        //        }
+        //        project.Users.Add(applicationUser);
+        //        DbContext.ProjectDatabase.Add(project);
+        //    }
+        //    else
+        //    {
+        //        project = DbContext.ProjectDatabase.FirstOrDefault(p => p.Projectid == id);
 
-                if (project == null)
-                {
-                    return RedirectToAction(nameof(HomeController.Index));
-                }
-            }
-            project.ProjectName = formData.ProjectName;
+        //        if (project == null)
+        //        {
+        //            return RedirectToAction(nameof(HomeController.Index));
+        //        }
+        //    }
+        //    project.ProjectName = formData.ProjectName;
 
-            project.DateUpdated = DateTime.Now;
-
-
-
-            DbContext.SaveChanges();
-
-            return RedirectToAction(nameof(HomeController.Index));
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin,ProjectManager")]
-        public ActionResult Edit(int? id)
-        {
-            if (!id.HasValue)
-            {
-                return RedirectToAction(nameof(HomeController.Index));
-            }
-
-            var project = DbContext.ProjectDatabase.FirstOrDefault(p => p.Projectid == id);
-
-            if (project == null)
-            {
-                return RedirectToAction(nameof(HomeController.Index));
-            }
-
-            var model = new CreateProjectViewModel();
-            model.ProjectName = project.ProjectName;
-            return View(model);
-        }
-
-        [HttpPost]
-        [Authorize(Roles = "Admin,ProjectManager")]
-        public ActionResult Edit(int id, CreateProjectViewModel formData)
-        {
-            return SaveProject(id, formData);
-        }
+        //    project.DateUpdated = DateTime.Now;
 
 
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //    DbContext.SaveChanges();
 
-            return View();
-        }
+        //    return RedirectToAction(nameof(HomeController.Index));
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //[HttpGet]
+        //[Authorize(Roles = "Admin,ProjectManager")]
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (!id.HasValue)
+        //    {
+        //        return RedirectToAction(nameof(HomeController.Index));
+        //    }
 
-            return View();
-        }
+        //    var project = DbContext.ProjectDatabase.FirstOrDefault(p => p.Projectid == id);
+
+        //    if (project == null)
+        //    {
+        //        return RedirectToAction(nameof(HomeController.Index));
+        //    }
+
+        //    var model = new CreateProjectViewModel();
+        //    model.ProjectName = project.ProjectName;
+        //    return View(model);
+        //}
+
+        //[HttpPost]
+        //[Authorize(Roles = "Admin,ProjectManager")]
+        //public ActionResult Edit(int id, CreateProjectViewModel formData)
+        //{
+        //    return SaveProject(id, formData);
+        //}
+
+
+
+        
     }
 }
---
+++ Mvc2/Models/ViewModel/ShowProjectViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc2.Models.ViewModel
+{
+    public class ShowProjectViewModel
+    {
+        public string ProjectName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        //public virtual ApplicationUser User { get; set; }
+    }
+}
